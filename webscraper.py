f79247d983c868420bc37b9c95699c14318919b6
Jackson Hayward <jchayward1@gmail.com>
2022-06-08 16:14:31 -0700

    Create outline for scraping progess

--- webscraper.py
+++ webscraper.py
@@ -3,15 +3,33 @@ This Python script is intended to retrieve important data from all trails listed
 website. 
 """
 
-def makePageLinks():
+# WTA has 132 pages of hikes, each page with 30 trails
+# This function creates a link to each page of 30
+
+def makePageList():
 
     link_format = 'https://www.wta.org/go-outside/hikes?b_start:int='
     all_links = []
 
     #132 pages of hikes
     for i in range(132):
-        pagelink = link_format + str(i * 30)
+        page_link = link_format + str(i * 30)
         all_links.append(pagelink)
-    print(len(all_links))
+    return all_links
 
-makePageLinks()
+
+
+# This function retrieves links to each trail on page containing 30 trails
+def getPage(all_links):
+    pass
+
+# This function finds the hike coordinates, name, and region for each link passed
+# and adds the data to a CSV file
+def findHikeData(trail_link):
+    pass
+
+
+
+#--------------- calling functions ---------------
+
+all_pages = makePageList()
