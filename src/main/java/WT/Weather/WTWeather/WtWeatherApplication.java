b9f80056649b8266776cc0bb16b70fcfb4caceba
Jackson Hayward <jchayward1@gmail.com>
2022-09-15 11:58:00 -0700

    Setting up repository

---
+++ src/main/java/WT/Weather/WTWeather/WtWeatherApplication.java
@@ -0,0 +1,13 @@
+package WT.Weather.WTWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WT/Weather/WTWeather/WtWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package WT.Weather.WTWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
